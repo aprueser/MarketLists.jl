bada4545dc119271ce4881243e5088e89969ddc8
Documenter.jl <documenter@juliadocs.github.io>
2022-04-25 06:54:47 +0000

    build based on a6ee4d2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MarketLists","category":"page"},{"location":"#MarketLists","page":"Home","title":"MarketLists","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MarketLists.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MarketLists]","category":"page"}]
+}
